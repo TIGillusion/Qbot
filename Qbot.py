06fda94d9d6ad981b41f7281e4484ca9140285b1
TIGillusion <127491557+TIGillusion@users.noreply.github.com>
2024-07-27 20:33:52 +0800

    Add files via upload

--- Qbot.py
+++ Qbot.py
@@ -1,6 +1,3 @@
-
-# 注意：如果启用了绘画或者语音合成等需要发送文件到qq的服务时，需要同时启动另一个名为 file.py 的程序 ！！！！
-
 debug = True
 print('\n欢迎使用由幻日团队-幻日编写的幻蓝AI程序，有疑问请联系q：2141073363')
 import ssl
@@ -15,7 +12,15 @@ import socket
 import re
 from threading import Thread
 import base64
-
+# os.system('start cqhttp')
+# print('程序启动中...')
+# time.sleep(10)
+# print('启动getnew辅助程序中...')
+# os.system('start /min getnews.exe')
+# time.sleep(5)
+# print('启动backnew辅助程序中...')
+# os.system('start /min backnews.exe')
+# time.sleep(1)
 def draw_group(prompt,to):
     try:
         urldraw=draw_url
@@ -61,6 +66,51 @@ def draw_group(prompt,to):
         print('绘画错误:',e)
         send_msg({'msg_type': 'group', 'number': to, 'msg':'AI绘画操作无法执行'})
 
+def draw_private(prompt,to):
+    try:
+        urldraw=draw_url
+        headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer "+draw_key
+            }
+        
+        data={
+            "model":draw_model,##claude-3-opus-vf
+            "messages":[{"role":"user","content":prompt}],
+            "stream": True
+        }
+        send_msg({'msg_type': 'private', 'number': to, 'msg': '正在绘画[%s]中...'%prompt})
+        response=requests.post(url=urldraw,headers=headers,stream=True,data=json.dumps(data))
+        if response.status_code==200:
+            send_msg({'msg_type': 'private', 'number': to, 'msg': '绘画完毕发送中...'})
+        processed_d_data_draw=''
+        for line in response.iter_lines():
+            try:
+                decoded=line.decode('utf-8').replace('\n','\\n').replace('\b','\\b').replace('\f','\\f').replace('\r','\\r').replace('\t','\\t')
+                if decoded != '':
+                    processed_d_data_draw+=json.loads(decoded[5:])["choices"][0]["delta"]["content"]
+                    print(decoded)
+            except Exception as e:
+                print(e)
+        image_url=processed_d_data_draw.split('(')[-1].replace(')','')
+        print(image_url)
+        max_n=500
+        for n in range(0,max_n):
+            try:
+                image_response=requests.get(image_url)
+                name=str(random.randrange(100000,999999))+'.png'
+                with open("./data/image/%s"%name,'wb') as f_image:
+                    f_image.write(image_response.content)
+                send_image({'msg_type': 'private', 'number': to, 'msg':name })
+                break
+            except:
+                print(n)
+                if n==max_n-1:
+                    raise TimeoutError("重试无效")
+    except Exception as e:
+        print('绘画错误:',e)
+        send_msg({'msg_type': 'private', 'number': to, 'msg':'AI绘画操作无法执行'})
+
 def send_msg(resp_dict):
     msg_type = resp_dict['msg_type']  # 回复类型（群聊/私聊）
     number = resp_dict['number']  # 回复账号（群号/好友号）
@@ -81,7 +131,30 @@ def send_msg(resp_dict):
 
 
 
-
+    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+    # ip = '127.0.0.1'
+    # client.connect((ip, 3000))
+ 
+    # msg_type = resp_dict['msg_type']  # 回复类型（群聊/私聊）
+    # number = resp_dict['number']  # 回复账号（群号/好友号）
+    # msg = resp_dict['msg']  # 要回复的消息
+    
+    # # 将字符中的特殊字符进行url编码
+    # msg = msg.replace(" ", "%20")
+    # msg = msg.replace("\n", "%0a")
+    # msg = msg.replace(";", "%3b")
+ 
+    # if msg_type == 'group':
+    #     payload = "GET /send_group_msg?group_id=" + str(
+    #         number) + "&message=" + msg + " HTTP/1.1\r\nHost:" + ip + ":5700\r\nConnection: close\r\n\r\n"
+    # elif msg_type == 'private':
+    #     payload = "GET /send_private_msg?user_id=" + str(
+    #         number) + "&message=" + msg + " HTTP/1.1\r\nHost:" + ip + ":5700\r\nConnection: close\r\n\r\n"
+    # print("发送" + payload)
+    # client.send(payload.encode("utf-8"))
+    # client.close()
+    # time.sleep(1)
     return 0
 
 def send_image(resp_dict):
@@ -149,7 +222,174 @@ def main(rev):
     global objdict
     try:
         if rev["message_type"] == "private":
-            pass #私聊没有开发，大家可根据群聊的自己开发，很简单，与群聊不同的就是 rev 里的信息格式不一样，其他的都差不多
+            if "illue%schat"%rev["sender"]["user_id"] not in objdict.keys():
+                objdict["illue%schat"%rev["sender"]["user_id"]]=""
+
+            
+            if "[CQ:image,"  not in rev['raw_message']:
+                objdict["illue%schat"%rev["sender"]["user_id"]]+=(rev["sender"]["nickname"]+"："+rev['raw_message'].replace('[CQ:at,qq=%d]'%rev['self_id'],'')+'\n\n')
+                objdict["illue%schat"%rev["sender"]["user_id"]]=objdict["illue%schat"%rev["sender"]["user_id"]][-50:]
+
+            if True:
+                a=objdict["illue%schat"%rev["sender"]["user_id"]]
+                print(a)
+                self_id=random.randrange(100000,999999)
+                objdict["illue%sgeneing"%rev["sender"]["user_id"]]=[self_id] 
+                rev['raw_message']=rev['raw_message'].replace('[CQ:at,qq=%d]'%rev['self_id'],'')
+                if "illue%s"%rev["sender"]["user_id"] not in objdict.keys():
+                    # for iii in range(1,settings+1):#设置人设总数量遍历
+                    #     exec("ilu%dname=json.load(open('setting.json','r',encoding='utf-8'))['wechat%d']['name']"%(iii,iii))
+                    #     exec("ilu%dinfo=json.load(open('setting.json','r',encoding='utf-8'))['wechat%d']['sets']"%(iii,iii))
+                    #     exec('print("人设%d已完成设定并启用")'%iii)
+                        # exec('ilu%dinfo=ilu%dinfo.replace(ilu%dname,"Assort")'%(iii,iii,iii))
+                    # exec('temp_prompt=ilu%dinfo'%random.randrange(1,settings+1))
+                    objdict["illue%s"%rev["sender"]["user_id"]]=[[{'role':'system','content':system}]]
+                if '#reset' in rev['raw_message']:
+                    objdict["illue%s"%rev["sender"]["user_id"]]=[[{'role':'system','content':system}]]
+                    send_msg({'msg_type': 'private', 'number': rev["sender"]["user_id"], 'msg': '已清空对话历史'}) 
+                else:        
+                    # data={"user_input":"你是一个意图分析助手，接下来我会给你一段文字，你需要分析用户这段话的意图，备选答案为[01:'想聊天',02:'向AI提问以寻求帮助',03:'进行AI绘画']，你只能从中选一个最贴切的答案用中文直接将序号回复给我，不允许说其他内容。"+"当前需要分析意图的语段是：“"+rev['raw_message']+"”","history":[{'role':'system','content':"你是一个意图分析助手，接下来我会给你一段文字，你需要分析用户这段话的意图，备选答案为[01:'想聊天',02:'向AI提问以寻求帮助',03:'进行AI绘画']，你只能从中选一个最贴切的答案直接回复序号给我，不允许说其他内容。"}]}
+                    # response1 = requests.post(
+                    #     "http://127.0.0.1:18080/messageGLM", stream=True, data=json.dumps(data)
+                    # )  # 发送请求判断意图
+                    # # print(response1.content)
+                    # for line in response1.iter_lines():
+                    #     decoded1 = (
+                    #         line.decode("unicode_escape")
+                    #         .replace("\b", "\\b")
+                    #         .replace("\f", "\\f")
+                    #         .replace("\r", "\\r")
+                    #         .replace("\t", "\\t")
+                    #         .replace("\n", "\\n")
+                    #     )
+                    #     processed_d_data = json.loads(decoded1)["stream_output"]
+                    processed_d_data="强制切换意图"
+                    print(processed_d_data)
+                    if not processed_d_data:
+                        processed_d_data='.'
+                    
+                    # data2={"user_input":objdict["illue%schat"%rev['group_id']],"history":objdict["illue%s"%rev['group_id']][0]}
+                    turl=user_api
+                    headers={
+                                "Content-Type": "application/json",
+                                "Authorization": "Bearer "+user_key
+                        }
+                    messages=objdict["illue%s"%rev["sender"]["user_id"]][0]+[{"role":"user","content":objdict["illue%schat"%rev["sender"]["user_id"]]}]
+                    data={
+                        "model": user_chat_model,##claude-3-opus-vf
+                        "messages":messages,
+                        "stream": True
+                    }
+                    response=requests.post(url=turl,headers=headers,stream=True,data=json.dumps(data))
+                # print(response1.content)
+                    temp_tts_list=[]
+                    processed_d_data1=''
+                    for line in response.iter_lines():
+                        try:
+                            decoded=line.decode('utf-8').replace('\n','\\n').replace('\b','\\b').replace('\f','\\f').replace('\r','\\r').replace('\t','\\t')
+                            if decoded != '':
+                                processed_d_data1+=json.loads(decoded[5:])["choices"][0]["delta"]["content"]
+                        except Exception as e:
+                            continue
+                            pass
+                        lastlen=len(temp_tts_list)
+                        # decoded1=line.decode('unicode_escape').replace('\n','\\n').replace('\b','\\b').replace('\f','\\f').replace('\r','\\r').replace('\t','\\t')
+                        # processed_d_data1=json.loads(decoded1)["stream_output"]
+                        if '```' not in processed_d_data1:
+                            temp_tts_list=processed_d_data1.split("#split#")
+                        else:
+                            temp_tts_list=[processed_d_data1]
+                        if not temp_tts_list:
+                            temp_tts_list=temp_tts_list[:-1]
+                        if self_id not in objdict["illue%sgeneing"%rev["sender"]["user_id"]]:
+                            objdict["illue%s"%rev["sender"]["user_id"]][0]=objdict["illue%s"%rev["sender"]["user_id"]][0]+[{'role':'user','content':rev['raw_message']},{'role':'assistant','content':processed_d_data1}]
+                            raise InterruptedError("新消息中断")
+                        
+                        if len(temp_tts_list)>1 and lastlen < len(temp_tts_list):
+                            # if ":" in temp_tts_list[-2]:
+                            #     if temp_tts_list[-2].split(":")[0] != "幻蓝":
+                            #         objdict["illue%s"%rev['group_id']][0]=[objdict["illue%s"%rev['group_id']][0][0]]
+                            #         break
+                            # elif "：" in temp_tts_list[-2]:
+                            #     if temp_tts_list[-2].split("：")[0] != "幻蓝":
+                            #         objdict["illue%s"%rev['group_id']][0]=[objdict["illue%s"%rev['group_id']][0][0]]
+                            #         break
+                            if '#voice/' in temp_tts_list[-2]:
+                                try:
+                                    voice=temp_tts_list[-2].split('#voice/')[-1].replace("#",'')
+                                    tts_data = {
+                                    "cha_name": "花火",
+                                    "text": voice.replace("...", "…").replace("…", ","),
+                                    "character_emotion":random.choice(['default','平常的','慢速病娇','傻白甜','平静的','疯批','聊天'])
+                                    }
+                                    b_wav = requests.post(
+                                        url='http://127.0.0.1:5000/tts', json=tts_data
+                                        )
+                                    n=random.randrange(10000,99999)
+                                    name='%stts%d.wav'%((time.strftime('%F')+'-'+time.strftime('%T').replace(':','-')),n)
+                                    to_path='./data/voice/%s'%name
+                                    with open(to_path,'wb') as wbf:
+                                        wbf.write(b_wav.content)
+                                    send_voice({'msg_type': 'private', 'number': rev["sender"]["user_id"], 'msg':name })
+                                except Exception as e:
+                                    send_msg({'msg_type': 'private', 'number': rev["sender"]["user_id"], 'msg': "语音合成失败"})
+                                    print("暂不支持语音合成")
+                            elif '#picture/' in temp_tts_list[-2]:
+                                picture=temp_tts_list[-2].split('#picture/')[-1].replace("#",'')
+                                print(picture)
+                                draw_private(picture,rev["sender"]["user_id"])
+                                
+                            else:
+                                send_msg({'msg_type': 'private', 'number': rev["sender"]["user_id"], 'msg': temp_tts_list[-2].replace("幻蓝：","").replace("幻蓝:","")})
+                    if "抱歉" in temp_tts_list[-1]:
+                        objdict["illue%s"%rev["sender"]["user_id"]][0]=[objdict["illue%s"%rev["sender"]["user_id"]][0][0]]
+                        print("催眠失败，重置记忆")
+                    # elif ":" in temp_tts_list[-1] or "：" in temp_tts_list[-1]:
+                    #     if temp_tts_list[-1].split(":")[0] != "幻蓝":
+                    #         objdict["illue%s"%rev['group_id']][0]=[objdict["illue%s"%rev['group_id']][0][0]]
+                    #     else:
+                    #         send_msg({'msg_type': 'group', 'number': rev['group_id'], 'msg': temp_tts_list[-1].replace("幻蓝：","").replace("幻蓝:","")})
+                    #     if temp_tts_list[-1].split("：")[0] != "幻蓝":
+                    #         objdict["illue%s"%rev['group_id']][0]=[objdict["illue%s"%rev['group_id']][0][0]]
+                    #     else:
+                    #         send_msg({'msg_type': 'group', 'number': rev['group_id'], 'msg': temp_tts_list[-1].replace("幻蓝：","").replace("幻蓝:","")})
+                    else:
+                        if '#voice/' in temp_tts_list[-1]:
+                            voice=temp_tts_list[-1].split('#voice/')[-1].replace("#",'')
+                            tts_data = {
+                                "cha_name": "花火",
+                                "text": voice.replace("...", "…").replace("…", ","),
+                                "character_emotion":random.choice(['default','平常的','慢速病娇','傻白甜','平静的','疯批','聊天'])
+                                }
+                            b_wav = requests.post(
+                                url='http://127.0.0.1:5000/tts', json=tts_data
+                                )
+                            n=random.randrange(10000,99999)
+                            name='%stts%d.wav'%((time.strftime('%F')+'-'+time.strftime('%T').replace(':','-')),n)
+                            to_path='./data/voice/%s'%name
+                            with open(to_path,'wb') as wbf:
+                                wbf.write(b_wav.content)
+                            send_voice({'msg_type': 'private', 'number':rev["sender"]["user_id"], 'msg':name })
+                        elif '#picture/' in temp_tts_list[-1]:
+                            picture=temp_tts_list[-1].split('#picture/')[-1].replace("#",'')
+                            print(picture)
+                            draw_private(picture,rev["sender"]["user_id"])
+                        else:
+                            send_msg({'msg_type': 'private', 'number': rev["sender"]["user_id"], 'msg': temp_tts_list[-1].replace("幻蓝：","").replace("幻蓝:","")})
+                    print(processed_d_data1)
+                    #send_msg({'msg_type': 'group', 'number': rev['group_id'], 'msg': processed_d_data1})
+                    objdict["illue%s"%rev["sender"]["user_id"]][0]=objdict["illue%s"%rev["sender"]["user_id"]][0]+[{'role':'user','content':rev["sender"]["user_id"]},{'role':'assistant','content':processed_d_data1}]
+                #objdict["illue%s"%rev['group_id']][1]=objdict["illue%s"%rev['group_id']][1]+[{'role':'user','content':rev['raw_message']},{'role':'assistant','content':processed_d_data1}]
+                    # rand=random.randrange(1,settings+1)
+                    # exec('objdict["illue%s"]=wechat(ilu%dname,ilu%dinfo,12,20,5,cmsg.from_user_id,"微信群聊%s")'%(cmsg.from_user_id,rand,rand,cmsg.from_user_nickname))
+                    # exec('objdict["illue%s"].ispersonal=False'%cmsg.from_user_id)
+                print("未发现新消息...运行时间：%f"%(time.time()-startT))
+                if len(objdict["illue%s"%rev["sender"]["user_id"]][0])> 8:
+                    objdict["illue%s"%rev["sender"]["user_id"]][0]=[objdict["illue%s"%rev["sender"]["user_id"]][0][0]]+objdict["illue%s"%rev["sender"]["user_id"]][0][-6:]
+                # if len(objdict["illue%s"%rev['group_id']][1])> 8:
+                #     objdict["illue%s"%rev['group_id']][1]=[objdict["illue%s"%rev['group_id']][1][0]]+objdict["illue%s"%rev['group_id']][1][-6:]    
+            objdict["illue%schat"%rev["sender"]["user_id"]]=''
+            
 
         elif rev["message_type"] == "group":
             if ("团子" in rev["sender"]["nickname"] or "芙芙" in rev["sender"]["nickname"]) and "[CQ:image," in rev['raw_message']:
@@ -160,7 +400,7 @@ def main(rev):
                 })
                 print("delete_msg:",rev['raw_message'],json.loads(res.content))
             if "芙芙" in rev["sender"]["nickname"] or "团子" in rev["sender"]["nickname"] or "芙宁娜" in rev["sender"]["nickname"]:
-                raise RuntimeError("break limitless turn") #防止机器人之间聊天刷屏
+                raise RuntimeError("break limitless turn")
             
 
             if "illue%schat"%rev['group_id'] not in objdict.keys():
@@ -247,7 +487,7 @@ def main(rev):
                         if '```' not in processed_d_data1:
                             temp_tts_list=processed_d_data1.split("#split#")
                         else:
-                            temp_tts_list=processed_d_data1
+                            temp_tts_list=[processed_d_data1]
                         if not temp_tts_list:
                             temp_tts_list=temp_tts_list[:-1]
                         if self_id not in objdict["illue%sgeneing"%rev['group_id']]:
@@ -272,7 +512,7 @@ def main(rev):
                                     "character_emotion":random.choice(['default','平常的','慢速病娇','傻白甜','平静的','疯批','聊天'])
                                     }
                                     b_wav = requests.post(
-                                        url='http://127.0.0.1:5000/tts', json=tts_data  #注意，语音合成服务可以使用AI桌宠里面附带的那个本地语音合成实现
+                                        url='http://127.0.0.1:5000/tts', json=tts_data
                                         )
                                     n=random.randrange(10000,99999)
                                     name='%stts%d.wav'%((time.strftime('%F')+'-'+time.strftime('%T').replace(':','-')),n)
@@ -311,7 +551,7 @@ def main(rev):
                                 "character_emotion":random.choice(['default','平常的','慢速病娇','傻白甜','平静的','疯批','聊天'])
                                 }
                             b_wav = requests.post(
-                                url='http://127.0.0.1:5000/tts', json=tts_data #注意，语音合成服务可以使用AI桌宠里面附带的那个本地语音合成实现
+                                url='http://127.0.0.1:5000/tts', json=tts_data
                                 )
                             n=random.randrange(10000,99999)
                             name='%stts%d.wav'%((time.strftime('%F')+'-'+time.strftime('%T').replace(':','-')),n)
